c3541bd89b5b0a0d7211ec0d971dd51589e1600e
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2025-12-17 18:48:53 +0700

    Add Railway deployment configuration

--- app.js
+++ app.js
@@ -45,6 +45,6 @@ app.use((req, res) => {
     res.status(404).render('error/404');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
