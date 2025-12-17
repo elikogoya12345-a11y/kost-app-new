7bd43d078c6ed8d8a900e0d4c95da0954c3226d9
Eligrah Philip Mapa Kogoya <elikogoya12345@gmail.com>
2025-12-18 04:57:18 +0700

    Clean up unused SQL and temporary files

--- .history/database/seed_20251218013714.sql
+++
@@ -1,2 +0,0 @@
-USE kost_professional;
-UPDATE users SET role = 'admin' WHERE email = 'testadmin123@test.com';
--- .history/database/seed_20251218015124.sql
+++
@@ -1,4 +0,0 @@
-USE kost_professional;
-UPDATE users 
-SET password = '$2a$10$AdFI1Gt4ru.Nj8w.6srR/eW0LTQm54TRAwbnFYN2H3MEhBrJKKxra' 
-WHERE username = 'admin';
